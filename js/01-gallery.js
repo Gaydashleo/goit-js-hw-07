efc0ca729612893e24847d415d940857b94ed1e7
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-15 19:14:10 +0300

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divEl = document.querySelector('.gallery');
 
 console.log(galleryItems);
+
+// 1 task 
+function createGalleryImage(items) {
+  return items.map(item =>  `<div class="gallery__item">
+      <a class="gallery__link" href='${item.original}'>
+      <img
+        class="gallery__image"
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'
+      />
+    </a>
+  </div>`)
+  .join(' ');
+    
+};
+const addGalleryImage = createGalleryImage(galleryItems);
+
+divEl.insertAdjacentHTML("afterbegin", addGalleryImage);
+
+// task 2 
+divEl.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+
+  const { target: elemImg } = event;
+  
+  const instance = basicLightbox.create(`
+    <img src='${elemImg.dataset.source}' alt = '${elemImg.alt}'>`)
+
+  instance.show()
+  if (elemImg.dataset.source === false) {
+    return;
+    
+  };
+  console.log(event);
+}
+
+// task Escape
+
+// function escapeModal(event) {
+//   if () {
+    
+//   }
+// };
+
+// document.addEventListener("keydown", escapeModal  {
+//   // console.log("key: ", event.key);
+//   // console.log("code: ", event.code);
+// });
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divEl = document.querySelector('.gallery');
 
 console.log(galleryItems);
+
+function createGalleryImage(items) {
+  return items.map(item =>  `<div class="gallery__item">
+      <a class="gallery__item" href='${item.original}'>
+      <img class="gallery__image" src='${item.preview}' alt='${item.description}' />
+      </a> 
+  </div>`)
+  .join(' ');
+    
+};
+
+const addGalleryImage = createGalleryImage(galleryItems);
+
+divEl.insertAdjacentHTML("afterbegin", addGalleryImage);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
+// task 2 
+// divEl.addEventListener('click', openModal);
+
+// function openModal(event) {
+//   event.preventDefault();
+//   const { target: elemImg } = event;
+//   console.log(event.target);
+//   console.log(event.currentTarget);
+//   const instance = basicLightbox.create(`
+//     <img src='${elemImg.dataset.source}' alt = '${elemImg.alt}'>`)
+
+//   instance.show()
+//   if (elemImg.dataset.source === false) {
+//     return;
+    
+//   };
+//   console.log(event);
+// }
